96134ee19506066b8d0af275da7805f9981964cf
imdizi <jhkgus@naver.com>
2011-06-22 16:48:15 +0900

    refectoring @gg_2

--- gg_2.c
+++ gg_2.c
@@ -1,14 +1,25 @@
 #include <stdio.h>
 
-int main(void){
-	int n;
-	int i;
+void getNumber(int *n){
+	scanf("%d",n);
+}
 
-	scanf("%d", &n);
+void printResult(int n){
+	int i;
 
 	for(i=1;i<=9;i++){
 		printf("%d * %d = %d\n",n,i,n*i);
 	}
+}
+
+int main(void){
+	int n;
+
+	getNumber(&n);
+	// get number from user
+
+	printResult(n);
+	// print result of "n*1" to "n*9"
 
 	return 0;
 }
